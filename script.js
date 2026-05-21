20bae418fd98691663567019c6d78cd185a693bd
Fernando Anaya <je_ss_y911@hotmail.com>
2026-05-20 19:39:16 -0600

    feat: funciones, condicionales y arrays en JavaScrip

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-let nombre = "Fernando";
-let edad = 31;
-let aprendiendo = true;
+let nombres = ["fernando", "maria", "carlos", "ana"];
 
-console.log(nombre);
-console.log(edad);
-console.log(aprendiendo);
+let nombresEnMayusculas = nombres.map(nombres => nombres.toUpperCase());
 
-let suma = edad + 10;
-console.log(suma);
-
-let saludo = "Hola, me llamo " + nombre;;
-console.log(saludo);
-
-let presentacion = "Me llamo " + nombre + " y tengo " + edad + "años.";
-console.log(presentacion);
+console.log(nombresEnMayusculas);
