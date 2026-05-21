f5ea6b86ccbc40521c037d281d8ad01155f37226
Fernando Anaya <je_ss_y911@hotmail.com>
2026-05-20 20:34:21 -0600

    drak/light mode y eventos de JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-let nombres = ["fernando", "maria", "carlos", "ana"];
+let botonProyectos = document.querySelector(".btn-hero");
+let botonTema = document.querySelector(".btn-tema");
+let body = document.querySelector("body");
 
-let nombresEnMayusculas = nombres.map(nombres => nombres.toUpperCase());
+let temaOscuro = true;
 
-console.log(nombresEnMayusculas);
+botonProyectos.addEventListener("click", function() {
+    alert("¡Aquí irán mis proyectos!");
+});
+
+botonTema.addEventListener("click", function() {
+    if (temaOscuro) {
+        body.style.backgroundColor = "#ffffff";
+        body.style.color = "#0F172A";
+        temaOscuro = false;
+    } else {
+        body.style.backgroundColor = "#0F172A";
+        body.style.color = "white";
+        temaOscuro = true;
+    }
+});
