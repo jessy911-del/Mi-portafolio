83fa8be17c4bdd1798ef56aa0f0753e734759d27
Fernando Anaya <je_ss_y911@hotmail.com>
2026-05-21 20:00:35 -0600

    feat: proyectos dinamicos con JavaScript

--- script.js
+++ script.js
@@ -9,11 +9,35 @@ botonenviar.addEventListener("click", function() {
 
     if (nombre === "" || email === "") {
         mensaje.textContent = "⚠️ Por favor llena todos los campos.";
-        mensaje.style.color = "ef4444";
+        mensaje.style.color = "#ef4444"; 
         return;
     }
 
     mensaje.textContent = "✅ ¡Gracias por contactarme, " + nombre + "! Pronto me pondré en contacto contigo." + email;
-    mensaje.style.color = "10b981";
+    mensaje.style.color = "#10b981"; 
+
+});
+
+let proyectos = [
+    { nombre: "Portafolio Personal", tecnologia: "HTML & CSS" },
+    { nombre: "App del clima", tecnologia: "JavaScript & API" },
+    { nombre: "Lista de tareas", tecnologia: "JavaScript" },
+    { nombre: "Tienda Online", tecnologia: "React" },
+    { nombre: "Blog Personal", tecnologia: "Node.js" },
+    { nombre: "Chat en tiempo real", tecnologia: "Socket.io" },
+
+];
+
+let grid = document.getElementById("grid-proyectos");
+
+proyectos.forEach(function(proyecto) {
+    let div = document.createElement("div");
+    div.classList.add("proyecto");
+
+    div.innerHTML = `
+    <h3>${proyecto.nombre}</h3>
+    <p>${proyecto.tecnologia}</p>
+`;
+    grid.appendChild(div);
 
 });
